66212b856c2ba6fcb15016282f8904cc636e4645
RiadBx <115518025+RiadBx@users.noreply.github.com>
2024-02-12 18:21:48 +0600

    trying to fix error

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,19 +1,12 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// import { getFirestore } from 'firebase/firestore'
 const firebaseConfig = {
-  apiKey: "AIzaSyBX5gWDgKw_2pjg5dcTcm4akSKBWNCkP2c",
+  apiKey: "AIzaSyBzZkaoPyJqL40HHwfK_CTjaDNBlhJFOy8",
   authDomain: "ride-sharing-project-auth.firebaseapp.com",
   projectId: "ride-sharing-project-auth",
   storageBucket: "ride-sharing-project-auth.appspot.com",
   messagingSenderId: "1064492167353",
   appId: "1:1064492167353:web:6887587ea91dd39672eb68",
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-export default app;
+export const app = initializeApp(firebaseConfig);
